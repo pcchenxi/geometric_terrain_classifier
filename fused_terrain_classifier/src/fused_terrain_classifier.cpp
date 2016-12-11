7c469f90a88e4f5748068e4d763757918d6774ab
xi <xi8@kth.se>
2016-12-11 15:38:29 +0100

    small bug

--- fused_terrain_classifier/src/fused_terrain_classifier.cpp
+++ fused_terrain_classifier/src/fused_terrain_classifier.cpp
@@ -344,7 +344,7 @@ void callback_cloud(const sensor_msgs::PointCloud2ConstPtr &cloud_in)
 {
     cout << "cloud in " << endl;
     cloud_recieved_map_ = transform_cloud(*cloud_in, map_frame, cloud_in->header.stamp);
-    cout << "points number:  " << cloud_recieved_map_.points.size() << endl;
+    // cout << "points number:  " << cloud_recieved_map_.points.size() << endl;
 
     initialized = true;
 }
